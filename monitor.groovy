4dd370df00ea67ad36752d08b4e6c6af867e3153
bhavitha1012 <55327840+bhavitha1012@users.noreply.github.com>
2021-04-21 12:49:13 +0530

    Update monitor.groovy

--- monitor.groovy
+++ monitor.groovy
@@ -1,6 +1,8 @@
+def func(){
 int exitcode = 0
 for (slave in hudson.model.Hudson.instance.slaves) {
  if (slave.getComputer().isOffline().toString() == "true"){
  println('Slave ' + slave.name + " is offline."); 
  }
+}
 }
