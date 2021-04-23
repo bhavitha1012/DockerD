cd4de6c127f0ba20b6d54bc2bd2d33d68d13e248
bhavitha1012 <55327840+bhavitha1012@users.noreply.github.com>
2021-04-23 14:29:14 +0530

    Update monitor.groovy

--- monitor.groovy
+++ monitor.groovy
@@ -1,9 +1,46 @@
-def func(){
-int exitcode = 0
-for (slave in hudson.model.Hudson.instance.slaves) {
- if (slave.getComputer().isOffline().toString() == "true"){
- println('Slave ' + slave.name + " is offline."); 
+import hudson.model.*    
+import jenkins.security.*
+import jenkins.model.*
+import hudson.security.*
+import hudson.model.User   
+
+
+  def rest(){
+    //getting the names of the jobs
+    def jobNames = []
+    Jenkins.instance.getAllItems(AbstractItem.class).each { 
+  jobNames.add(it.fullName) 
+}
+
+// For each project
+for(item in Hudson.instance.items) {
+  for(jobName in jobNames){
+    if(item.name.equalsIgnoreCase(jobName))
+    {
+        Set<String> users = new HashSet<>();
+      //adding the users to give access
+        users.add('jenkins-user');
+   
+         echo "providing access of the job:${jobName} to the user:jenkins-user"
+
+       Map<Permission,Set<String>> newPermissions = new HashMap<Permission, Set<String>>()
+       newPermissions.put(Item.READ,users)
+       newPermissions.put(hudson.model.Item.CANCEL, users)
+       newPermissions.put(Item.WORKSPACE, users);
+       newPermissions.put(Item.BUILD, users);
+       newPermissions.put(Run.DELETE, users);
+       newPermissions.put(Run.UPDATE, users);
+       newPermissions.put(Item.CONFIGURE, users);
+       newPermissions.put(Item.DELETE, users);
+       newPermissions.put(Item.READ, users);
+       item.addProperty(new AuthorizationMatrixProperty(newPermissions))
+       item.save()
+
+      
+      
+    }
+  }
  }
-}
-}
+     
+  }
 return this
