ba286ed41e97ac8cfd0c5c2ba24d3168da917269
bhavitha1012 <55327840+bhavitha1012@users.noreply.github.com>
2021-04-27 15:30:42 +0530

    Update monitor.groovy

--- monitor.groovy
+++ monitor.groovy
@@ -1,46 +1,47 @@
-import hudson.model.*
+import hudson.model.*    
 import jenkins.security.*
 import jenkins.model.*
 import hudson.security.*
-import hudson.model.User
-  def rest(job){
+import hudson.model.User   
+  def rest(arg_jobname){
     //getting the names of the jobs
     def jobNames = []
-    Jenkins.instance.getAllItems(AbstractItem.class).each {
-  jobNames.add(it.fullName)
+    Jenkins.instance.getAllItems(AbstractItem.class).each { 
+  jobNames.add(it.fullName) 
 }
+    def jobName_dynamic = []
+    jobName_dynamic.add(arg_jobname)
 // For each project
-    def test1=[]
-    test1.add(job)
 for(item in Hudson.instance.items) {
-  for(jobName in test1){
+  for(jobName in jobName_dynamic){
     if(item.name.equalsIgnoreCase(jobName))
     {
-        Set<String> users = new HashSet<>();
-      //adding the users to give access
-        users.add('jenkins-user1');
       echo "providing access of the job:${jobName} to the user:jenkins-user1"
-     // println(item+"to user2")
-      //providing all the permissions
-       Map<Permission,Set<String>> newPermissions = new HashMap<Permission, Set<String>>()
-       newPermissions.put(Item.READ,users)
-       newPermissions.put(hudson.model.Item.CANCEL, users)
-       newPermissions.put(Item.WORKSPACE, users);
-       newPermissions.put(Item.BUILD, users);
-       newPermissions.put(Run.DELETE, users);
-       newPermissions.put(Run.UPDATE, users);
-       newPermissions.put(Item.CONFIGURE, users);
-       newPermissions.put(Item.DELETE, users);
-       newPermissions.put(Item.READ, users);
-       newPermissions.put(com.cloudbees.plugins.credentials.CredentialsProvider.VIEW, users);
-       item.addProperty(new AuthorizationMatrixProperty(newPermissions))
-       item.save()
+       def userIDs = ['jenkins-user1']
+     // Adding the users and getting user name and user id
+        for(userID in userIDs){
+        User user = User.getOrCreateByIdOrFullName(userID)
+        String sID = user.getId() 
+        def authorizationMatrixProperty = item.getProperty(AuthorizationMatrixProperty.class)
+        authorizationMatrixProperty?.add(hudson.model.Item.CANCEL, sID)
+        authorizationMatrixProperty?.add(Item.WORKSPACE, sID);
+        authorizationMatrixProperty?.add(Item.BUILD, sID);
+        authorizationMatrixProperty?.add(Run.DELETE, sID);
+        authorizationMatrixProperty?.add(Run.UPDATE, sID);
+        authorizationMatrixProperty?.add(Item.CONFIGURE, sID);
+        authorizationMatrixProperty?.add(Item.DELETE, sID);
+        authorizationMatrixProperty?.add(Item.READ, sID);
+        authorizationMatrixProperty?.add(com.cloudbees.plugins.credentials.CredentialsProvider.VIEW, sID);
+        item.addProperty(authorizationMatrixProperty)
+        item.save()
+        }
     }
   }
  }
     //getting all the user
-    /*User.getAll().each { user ->
+    /*User.getAll().each { user ->   
       println user}
    */
+     
   }
 return this
