3293683dfb3737ccdb604bb3348c12d6bc5b4062
bhavitha1012 <55327840+bhavitha1012@users.noreply.github.com>
2021-04-27 10:28:38 +0530

    Update monitor.groovy

--- monitor.groovy
+++ monitor.groovy
@@ -1,29 +1,27 @@
-import hudson.model.*    
+import hudson.model.*
 import jenkins.security.*
 import jenkins.model.*
 import hudson.security.*
-import hudson.model.User   
-
-
-  def rest(Jobs){
+import hudson.model.User
+  def rest(job){
     //getting the names of the jobs
     def jobNames = []
-    Jenkins.instance.getAllItems(AbstractItem.class).each { 
-  jobNames.add(it.fullName) 
+    Jenkins.instance.getAllItems(AbstractItem.class).each {
+  jobNames.add(it.fullName)
 }
-def jobs1=[]
-    jobs1.add(Jobs)
 // For each project
+    def test1=[]
+    test1.add(job)
 for(item in Hudson.instance.items) {
-  for(jobName in jobs1){
+  for(jobName in test1){
     if(item.name.equalsIgnoreCase(jobName))
     {
         Set<String> users = new HashSet<>();
       //adding the users to give access
-        users.add('jenkins-user');
-   
-         echo "providing access of the job:${jobName} to the user:jenkins-user"
-
+        users.add('user1');
+      echo "providing access of the job:${jobName} to the user:user1"
+     // println(item+"to user2")
+      //providing all the permissions
        Map<Permission,Set<String>> newPermissions = new HashMap<Permission, Set<String>>()
        newPermissions.put(Item.READ,users)
        newPermissions.put(hudson.model.Item.CANCEL, users)
@@ -36,12 +34,12 @@ for(item in Hudson.instance.items) {
        newPermissions.put(Item.READ, users);
        item.addProperty(new AuthorizationMatrixProperty(newPermissions))
        item.save()
-
-      
-      
     }
   }
  }
-     
+    //getting all the user
+    /*User.getAll().each { user ->
+      println user}
+   */
   }
 return this
